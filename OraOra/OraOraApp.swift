a21336437eb118f463e85045d420a608b519e2d4
naoto-ueda <uettyan@gmail.com>
2021-12-21 09:10:39 +0900

    Initial Commit

---
+++ OraOra/OraOraApp.swift
@@ -0,0 +1,17 @@
+//
+//  OraOraApp.swift
+//  OraOra
+//
+//  Created by 植田直人 on 2021/12/21.
+//
+
+import SwiftUI
+
+@main
+struct OraOraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
