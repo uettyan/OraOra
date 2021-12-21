7ea1ce31c505275a2627d408f65dec5fa5933585
naoto-ueda <uettyan@gmail.com>
2021-12-21 09:56:14 +0900

    タップ座標を検知するように修正

--- OraOra/ContentView.swift
+++ OraOra/ContentView.swift
@@ -9,11 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Color.white.onTapGesture {
-            Text("Ora!")
-                .padding()
-            print("tap")
-        }
+        Color.white.gesture(DragGesture(minimumDistance: 0).onEnded({ (value) in
+            print(value.location)
+        }))
     }
 }
 
